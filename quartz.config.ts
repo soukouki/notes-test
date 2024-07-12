688a7221f4c4bcfde81bd39c97b1f7734855d444
soukouki <soukouki0@yahoo.co.jp>
2024-07-13 01:54:17 +0900

    ExplicitPublishがあるならRemoveDraftsは要らないだろうということで削除

--- quartz.config.ts
+++ quartz.config.ts
@@ -73,7 +73,6 @@ const config: QuartzConfig = {
       Plugin.Latex({ renderEngine: "katex" }),
     ],
     filters: [
-      Plugin.RemoveDrafts(),
       Plugin.ExplicitPublish(),
     ],
     emitters: [
